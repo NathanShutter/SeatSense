8c09f813b9d866b99ad479aae818d088c61a52b7
Jesse Quier <jquie803@live.kutztown.edu>
2024-03-30 16:40:56 -0400

    resized top blocks

--- frontend/src/components/DashboardPage/dashboard.js
+++ frontend/src/components/DashboardPage/dashboard.js
@@ -161,27 +161,27 @@ export default function Dashboard() {
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-              {/* Chart */}
+              {/* Welcome block (Chart) */}
               <Grid item xs={12} md={8} lg={3}>
                 <Paper
                   sx={{
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
+                    height: 200,
                   }}
                 >
                   <Chart />
                 </Paper>
               </Grid>
-              {/* Recent Deposits */}
+              {/* Current setting (Deposits) */}
               <Grid item xs={12} md={4} lg={9}>
                 <Paper
                   sx={{
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
+                    height: 200,
                   }}
                 >
                   <Deposits />
